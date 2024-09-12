5988e86ed69439520261c0ff47878370a9d67ddb
devgeon <devgeon.git@gmail.com>
2024-09-12 02:21:41 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# syntax=docker/dockerfile:1
+FROM openjdk:21
+
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/test/java/com/devgeon/yourlog/YourlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgeon.yourlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
