074ade28dcfbf6cf5e1f4f43238a249b4b69536e
devgeon <devgeon.git@gmail.com>
2024-09-14 16:03:04 +0900

    Feature: Add domain entities

---
+++ src/main/java/com/devgeon/yourlog/domain/Comment.java
@@ -0,0 +1,44 @@
+package com.devgeon.yourlog.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id", nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id", nullable = false, updatable = false)
+    private Article article;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+}
---
+++ src/main/java/com/devgeon/yourlog/domain/User.java
@@ -0,0 +1,42 @@
+package com.devgeon.yourlog.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
