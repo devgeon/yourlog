40381eacd4af6e42d22a924c9e4ebfff2c04ce62
devgeon <devgeon.git@gmail.com>
2024-09-15 11:13:37 +0900

    Test: Add repository test for user

---
+++ src/test/java/com/devgeon/yourlog/repository/UserRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.devgeon.yourlog.repository;
+
+import com.devgeon.yourlog.domain.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
+@Transactional
+@SpringBootTest
+class UserRepositoryTest {
+
+    final String USERNAME = "testUser", EMAIL = "test@test.com", PASSWORD = "testPassword";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void userSaveSuccess() {
+        // given
+
+        // when
+        User user = userRepository.save(User.builder().email(EMAIL).username(USERNAME).password(PASSWORD).build());
+
+        // then
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getEmail()).isEqualTo(EMAIL);
+        assertThat(user.getUsername()).isEqualTo(USERNAME);
+        assertThat(user.getPassword()).isEqualTo(PASSWORD);
+        assertThat(user.getCreatedAt()).isNotNull();
+        assertThat(user.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    void userSaveFailByDuplicatedEmail() {
+        // given
+
+        // when
+        userRepository.save(User.builder().email(EMAIL).username(USERNAME).password(PASSWORD).build());
+
+        // then
+        assertThrows(DataIntegrityViolationException.class, () ->
+                userRepository.save(User.builder().email(EMAIL).username(USERNAME).password(PASSWORD).build()));
+    }
+
+}
