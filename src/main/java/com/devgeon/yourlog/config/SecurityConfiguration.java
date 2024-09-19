d2c65b1ce39cc3ab954ed2afb96a1fc0702896a9
devgeon <devgeon.git@gmail.com>
2024-09-19 16:39:04 +0900

    Chore: Update dependencies to include spring security

---
+++ src/main/java/com/devgeon/yourlog/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.devgeon.yourlog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
+                .csrf((csrf) -> csrf.disable());
+
+        return http.build();
+    }
+}
